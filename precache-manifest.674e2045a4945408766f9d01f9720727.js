eabd35d7a7ddd57380eeb4acd17c2719d4b584bb
INOCcent-afk <michael828inoc@gmail.com>
2020-10-18 18:19:12 +0800

    Updates

---
+++ precache-manifest.674e2045a4945408766f9d01f9720727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740ca61c5514741743b3832b1a0af0ea",
+    "url": "/trvl/index.html"
+  },
+  {
+    "revision": "f69dde289e2b9141f507",
+    "url": "/trvl/static/css/main.226f3d69.chunk.css"
+  },
+  {
+    "revision": "8f2a927a0d9d9cc36420",
+    "url": "/trvl/static/js/2.57e583ee.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trvl/static/js/2.57e583ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f69dde289e2b9141f507",
+    "url": "/trvl/static/js/main.0a548072.chunk.js"
+  },
+  {
+    "revision": "4cb96473a1e193c73c37",
+    "url": "/trvl/static/js/runtime-main.35e7fd7a.js"
+  }
+]);
